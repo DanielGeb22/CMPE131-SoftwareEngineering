ea2b33fd29a67683bf8c17990c7f33595b520c91
Daniel Gebran <danielgebran92@gmail.com>
2025-03-18 20:47:49 -0700

    Final version of data parsing assignment 3/18

--- convert_txt_to_data_formats/convert_txt_to_data_formats.py
+++ convert_txt_to_data_formats/convert_txt_to_data_formats.py
@@ -3,14 +3,18 @@ import dicttoxml
 import os
 
 def convert(file, format):
+    # Defines current directory to save the converted files
     input_dir = os.path.dirname(os.path.abspath(file))
+    # Reads the TXT file
     file_read = pd.read_csv(file, delimiter="\t", encoding="ISO-8859-1", na_values=["#N/A"])
-    # file_read.columns = file_read.columns.str.strip()
 
+    # Convert TXT to CSV
     if (format == 'c'):
         file_read.to_csv(os.path.join(input_dir, "data.csv"), index=False)
+    # Convert TXT to JSON
     elif (format == 'j'):
         file_read.to_json(os.path.join(input_dir, "data.json"), orient="records", indent=4)
+    # Convert TXT to XML
     elif (format == 'x'):
         data_dict = file_read.to_dict(orient="records")
         xml_data = dicttoxml.dicttoxml(data_dict, custom_root="Players", attr_type=False)
@@ -18,6 +22,7 @@ def convert(file, format):
         xml_path = os.path.join(input_dir, "data.xml")
         with open(xml_path, "wb") as f:
             f.write(xml_data)
+    # If none of the above, print error
     else:
         print("Invalid format, please try again")
 
